ba460a4ef99b37347815ce2960cc3ec1b6d4c362
Marcos Martins <contato@marcosmartins.me>
2022-05-08 11:07:45 -0300

    :sparkles: Creates the CLI.

---
+++ right/__main__.py
@@ -0,0 +1,5 @@
+from right.console.cli import cli
+
+
+if __name__ == '__main__':
+    cli.run()
---
+++ right/console/cli.py
@@ -0,0 +1,4 @@
+from cleo.application import Application
+
+
+cli = Application(name = 'Right', version = '0.1.0')
